68f3145ec81416678ec4ca9ad639dab093ebf375
kannathasansdet <kannathasansdet@gmail.com>
2026-01-19 08:05:17 +0530

    Day1 Code

---
+++ Day1/DataTypes.js
@@ -0,0 +1,36 @@
+var age = 30;
+
+console.log("Hi Kanna ! Your age is  " + age);
+
+console.log(typeof age)
+
+var price = 19.99;
+
+var name = `Tharani`;
+
+console.log(typeof name)
+
+var status = "True";
+
+console.log(typeof status)
+
+var isValid = false;
+
+console.log(typeof isValid)
+
+
+var firstName ; // Declaration
+
+console.log(typeof firstName)
+
+
+firstName = "Kannathasan"; // Initialization
+
+console.log(typeof firstName)
+
+
+var middelName; // If there is no value for a particular variable
+
+var middelName = null;
+
+console.log(typeof middelName)
