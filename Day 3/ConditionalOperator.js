c718683b80c9f5c5389352716b3396af67ce3590
kannathasansdet <kannathasansdet@gmail.com>
2026-01-21 07:57:42 +0530

    Comparison and Conditional Operator

---
+++ Day 3/ConditionalOperator.js
@@ -0,0 +1,27 @@
+let age = 20;
+
+let result = (age >= 18) ? "Eligible to vote" : "Not eligible to vote";
+
+console.log(result);
+
+ // Milk product if incase price 30 above or else not milk product
+
+let price = 29;
+
+let product = (price>30) ? "MILK PRODUCT" : "NOT MILK PRODUCT";
+
+console.log(product);
+
+
+let marks = 75;
+
+let grade = (marks >= 50) ? "Pass" : "Fail";
+
+console.log(grade);
+
+let isLoggedIn = false;
+
+let message = isLoggedIn ? "Welcome User" : "Please Login";
+
+console.log(message);
+
---
+++ Day 3/LogicalOperator.js
@@ -0,0 +1,31 @@
+// Logical Operators Example Program
+
+let age = 22;
+let hasVoterID = true;
+let isStudent = false;
+
+// Logical AND (&&)
+console.log("AND Operator:");
+console.log(age < 18 && hasVoterID); // true
+
+// Logical OR (||)
+console.log("OR Operator:");
+console.log(age > 18 || isStudent); // false
+
+// Logical NOT (!)
+console.log("NOT Operator:");
+console.log(!hasVoterID); // false
+
+// Real-time example using if condition
+if (age >= 18 && hasVoterID) {
+  console.log("Eligible to vote");
+} else {
+  console.log("Not eligible to vote");
+}
+
+// Combination of logical operators
+if ((age >= 18 && hasVoterID) || isStudent) {
+  console.log("Access Granted");
+} else {
+  console.log("Access Denied");
+}
